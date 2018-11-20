7d4023ac1c9ed6c1c3f9b09ed8d495eda44c799c
Cameron MacLeod <cmacleod170@googlemail.com>
2018-11-20 20:33:12 +0000

    Add firebase user auth
    Remove old login screen design since unnecessary

--- app/src/main/java/com/cameronmacleod/coinz/MainActivity.kt
+++ app/src/main/java/com/cameronmacleod/coinz/MainActivity.kt
@@ -1,33 +1,70 @@
 package com.cameronmacleod.coinz
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    val RC_SIGN_IN = 42
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val coinzDbName : String = getString(R.string.db_name)
-        val db = applicationContext.getSharedPreferences(coinzDbName, Context.MODE_PRIVATE)
-        if (db.getBoolean("isLoggedIn", false)) {
-            val toast = Toast.makeText(applicationContext, "Logged in", Toast.LENGTH_SHORT)
-            toast.show()
-        }
+
         setContentView(R.layout.activity_main)
+
+        val user = FirebaseAuth.getInstance().currentUser
+
+        if (user != null) {
+            fetchData()
+            redirectToMainScreen()
+        }
     }
 
     fun onLoginButtonClicked(view: View) {
-        val password = passwordInput.text
-        val toast = Toast.makeText(applicationContext, password, Toast.LENGTH_SHORT)
-        toast.show()
-        val coinzDbName : String = getString(R.string.db_name)
-        val db = applicationContext.getSharedPreferences(coinzDbName, Context.MODE_PRIVATE)
-        val editable = db.edit()
-        editable.putBoolean("isLoggedIn", true)
-        editable.commit()
+        val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN)
+    }
+
+    fun fetchData() {
+
+    }
+
+    fun redirectToMainScreen() {
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = FirebaseAuth.getInstance().currentUser
+                fetchData()
+                redirectToMainScreen()
+            } else if (response != null) {
+                // if response was null, then user pressed the back button
+                val toast = Toast.makeText(applicationContext,
+                        "Login failed, please try again", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        }
     }
 }
