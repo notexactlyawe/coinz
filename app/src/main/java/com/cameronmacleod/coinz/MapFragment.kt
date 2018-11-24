cd63aaa555e63af4ab94946afa6ca06cb39495db
Cameron MacLeod <cmacleod170@googlemail.com>
2018-11-24 22:36:00 +0000

    Add default map

--- app/src/main/java/com/cameronmacleod/coinz/MapFragment.kt
+++ app/src/main/java/com/cameronmacleod/coinz/MapFragment.kt
@@ -1,5 +1,6 @@
 package com.cameronmacleod.coinz
 
+import android.app.Activity
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
@@ -7,6 +8,9 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.MapView
+
 
 /**
  * A simple [Fragment] subclass.
@@ -17,11 +21,30 @@ import android.view.ViewGroup
  */
 class MapFragment : Fragment() {
     private var listener: OnFragmentInteractionListener? = null
+    private lateinit var mapView: MapView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (activity != null) {
+            val nonNullActivity = activity as Activity
+            Mapbox.getInstance(nonNullActivity.applicationContext, "pk.eyJ1Ijoibm90ZXhhY3RseWF3ZSIsImEiOiJjam92enp0b2sxbjdqM3JucmJxbGcxbzI1In0.zNr5YfYwwBzpkp_ReEw2-w");
+        }
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (activity != null) {
+            val nonNullActivity = activity as Activity
+            mapView = nonNullActivity.findViewById(R.id.mapView) as MapView
+            mapView.onCreate(savedInstanceState)
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -42,4 +65,40 @@ class MapFragment : Fragment() {
         super.onDetach()
         listener = null
     }
+
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
 }
