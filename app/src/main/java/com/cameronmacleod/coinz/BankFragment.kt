71e9e13375bae4388de404d8eff83ab4c12fb075
Cameron MacLeod <cmacleod170@googlemail.com>
2018-12-03 20:28:09 +0000

    Enable conversion of coins
    Also fills balances

--- app/src/main/java/com/cameronmacleod/coinz/BankFragment.kt
+++ app/src/main/java/com/cameronmacleod/coinz/BankFragment.kt
@@ -1,6 +1,7 @@
 package com.cameronmacleod.coinz
 
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
@@ -16,8 +17,11 @@ import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
+import org.json.JSONException
+import org.json.JSONObject
+import java.text.DecimalFormat
 
-class BankFragment : Fragment() {
+class BankFragment : Fragment(), View.OnClickListener {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
@@ -25,11 +29,17 @@ class BankFragment : Fragment() {
     private lateinit var originalCoins: Coins
     private lateinit var bank: Bank
     private lateinit var progressOverlay: View
+    private lateinit var fragmentView: View
+    private var exchangeRates = hashMapOf(
+            "DOLR" to 0.0,
+            "SHIL" to 0.0,
+            "QUID" to 0.0,
+            "PENY" to 0.0)
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_bank, container, false)
+        fragmentView = inflater.inflate(R.layout.fragment_bank, container, false)
 
         originalCoins = (activity as MainActivity).coins
 
@@ -40,19 +50,26 @@ class BankFragment : Fragment() {
         viewManager = LinearLayoutManager(activity)
         viewAdapter = CollectedCoinsAdapter(collectedCoins, ::bankCoin)
 
-        recyclerView = view.findViewById<RecyclerView>(R.id.collectedCoinsList).apply {
+        recyclerView = fragmentView.findViewById<RecyclerView>(R.id.collectedCoinsList).apply {
             // improves performance since each element is same size
             setHasFixedSize(true)
             layoutManager = viewManager
             adapter = viewAdapter
         }
 
-        progressOverlay = view.findViewById(R.id.progress_overlay)
+        progressOverlay = fragmentView.findViewById(R.id.progress_overlay)
         animateProgressBarIn()
+
+        fillRates()
+
+        fragmentView.findViewById<Button>(R.id.convertQuid).setOnClickListener(this)
+        fragmentView.findViewById<Button>(R.id.convertPeny).setOnClickListener(this)
+        fragmentView.findViewById<Button>(R.id.convertDolr).setOnClickListener(this)
+        fragmentView.findViewById<Button>(R.id.convertShil).setOnClickListener(this)
 
         val userID = (activity as MainActivity).userID!!
         getOrCreateBank(userID)
-        return view
+        return fragmentView
     }
 
     private fun getOrCreateBank(userID: String) {
@@ -67,13 +84,38 @@ class BankFragment : Fragment() {
                         bank = task.result?.toObject(Bank::class.java)!!
                     }
                     animateProgressBarOut()
+                    setBalanceLabels()
                 }
                 .addOnFailureListener {
                     Log.d("FireStore", "Couldn't get $userID from bank table\n$it")
                     bank = Bank(name=userID)
                     updateBank(bank)
                     animateProgressBarOut()
+                    setBalanceLabels()
                 }
+    }
+
+    private fun fillRates() {
+        val sharedPrefs = activity?.getSharedPreferences(getString(R.string.preference_file_key),
+                Context.MODE_PRIVATE)
+        if (sharedPrefs == null) {
+            return
+        }
+        val geojsonStr = sharedPrefs.getString(getString(R.string.coinz_map_key), "")
+        val geojson = JSONObject(geojsonStr)
+
+        try {
+            val rates = geojson.getJSONObject("rates")
+
+            exchangeRates["SHIL"] = rates.getString("SHIL").toDouble()
+            exchangeRates["DOLR"] = rates.getString("DOLR").toDouble()
+            exchangeRates["QUID"] = rates.getString("QUID").toDouble()
+            exchangeRates["PENY"] = rates.getString("PENY").toDouble()
+        } catch (e: JSONException) {
+            val toast = Toast.makeText(activity, R.string.no_geojson_toast, Toast.LENGTH_LONG)
+            toast.show()
+            Log.e(this.javaClass.simpleName, "Error getting rates from geojson: ${e.localizedMessage}")
+        }
     }
 
     private fun animateProgressBarIn() {
@@ -91,6 +133,15 @@ class BankFragment : Fragment() {
         progressOverlay.setAnimation(outAnimation)
         progressOverlay.setVisibility(View.GONE)
         activity!!.window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+    }
+
+    private fun setBalanceLabels() {
+        val format = DecimalFormat("####.##")
+        fragmentView.findViewById<TextView>(R.id.shilAmount).text = format.format(bank.shilBalance)
+        fragmentView.findViewById<TextView>(R.id.quidAmount).text = format.format(bank.quidBalance)
+        fragmentView.findViewById<TextView>(R.id.dolrAmount).text = format.format(bank.dolrBalance)
+        fragmentView.findViewById<TextView>(R.id.penyAmount).text = format.format(bank.penyBalance)
+        fragmentView.findViewById<TextView>(R.id.goldAmount).text = format.format(bank.goldBalance)
     }
 
     private fun bankCoin(position: Int) {
@@ -112,6 +163,9 @@ class BankFragment : Fragment() {
 
         updateBank(bank)
 
+        // update display of balance
+        setBalanceLabels()
+
         // mark coin as banked
         coin.banked = true
 
@@ -121,6 +175,34 @@ class BankFragment : Fragment() {
         collectedCoins.removeAt(position)
         // can't notifyItemRemoved because messes with existing callbacks
         viewAdapter.notifyDataSetChanged()
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.convertDolr -> {
+                bank.goldBalance +=
+                        bank.dolrBalance * exchangeRates.getOrDefault("DOLR", 0.0)
+                bank.dolrBalance = 0.0
+            }
+            R.id.convertShil -> {
+                bank.goldBalance +=
+                        bank.shilBalance * exchangeRates.getOrDefault("SHIL", 0.0)
+                bank.shilBalance = 0.0
+            }
+            R.id.convertPeny -> {
+                bank.goldBalance +=
+                        bank.penyBalance * exchangeRates.getOrDefault("PENY", 0.0)
+                bank.penyBalance = 0.0
+            }
+            R.id.convertQuid -> {
+                bank.goldBalance +=
+                        bank.quidBalance * exchangeRates.getOrDefault("QUID", 0.0)
+                bank.quidBalance = 0.0
+            }
+        }
+
+        updateBank(bank)
+        setBalanceLabels()
     }
 }
 
