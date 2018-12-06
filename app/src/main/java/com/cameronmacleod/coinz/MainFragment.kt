7eae666c6cbd35ae717de4e6a5b6dc4c751c8b86
Cameron MacLeod <cmacleod170@googlemail.com>
2018-12-06 16:24:53 +0000

    RecyclerView for nearest coins in blind mode
    Fixes bug where having fewer than 4 coins left to collect would crash the app
    Cleans code

--- app/src/main/java/com/cameronmacleod/coinz/MainFragment.kt
+++ app/src/main/java/com/cameronmacleod/coinz/MainFragment.kt
@@ -1,5 +1,6 @@
 package com.cameronmacleod.coinz
 
+import android.app.Activity
 import android.content.Context
 import android.graphics.Color
 import android.location.Location
@@ -8,30 +9,43 @@ import android.location.LocationManager
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.content.ContextCompat
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TableRow
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
-import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.android.synthetic.main.fragment_main.*
 import org.json.JSONException
 import org.json.JSONObject
 
 /**
  * Fragment that implements blind mode. Shows nearest 4 coins and exchange rates for the day
  */
-class MainFragment : Fragment(), View.OnClickListener {
+class MainFragment : Fragment() {
     private var currLocation: Location? = null
-    private val stringIconMap = hashMapOf(
-            "DOLR" to R.drawable.ic_dolr,
-            "SHIL" to R.drawable.ic_shil,
-            "PENY" to R.drawable.ic_peny,
-            "QUID" to R.drawable.ic_quid)
-    private var topFourCoins = arrayOf("", "", "", "")
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: BlindModeAdapter
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private var topFourCoins = listOf<Coin>()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
+
+        viewManager = LinearLayoutManager(activity)
+        viewAdapter = BlindModeAdapter(topFourCoins, ::getLocation, ::onClick, activity!!)
+
+        recyclerView = view.findViewById<RecyclerView>(R.id.nearestFourCoins).apply {
+            // improves performance since each element is same size
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
 
         // Acquire a reference to the system Location Manager
         val locationManager = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
@@ -70,19 +84,12 @@ class MainFragment : Fragment(), View.OnClickListener {
             val toast = Toast.makeText(activity, "Please enable location", Toast.LENGTH_LONG)
             toast.show()
         }
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.content_main, container, false)
-
-        view.findViewById<TableRow>(R.id.coinRow1).setOnClickListener(this)
-        view.findViewById<TableRow>(R.id.coinRow2).setOnClickListener(this)
-        view.findViewById<TableRow>(R.id.coinRow3).setOnClickListener(this)
-        view.findViewById<TableRow>(R.id.coinRow4).setOnClickListener(this)
 
         return view
+    }
+
+    private fun getLocation(): Location {
+        return currLocation!!
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -128,93 +135,27 @@ class MainFragment : Fragment(), View.OnClickListener {
     private fun fillFourNearestCoins() {
         if (currLocation == null || activity == null) {
             // can't do anything until location comes in or if we have no attached activity
+            Log.d(javaClass.simpleName, "currLocation null? ${currLocation==null}")
             return
         }
 
         val coins = (activity as MainActivity).coins ?: return
         // sort based on distance
-        coins.coins.sortedBy {
-            distanceToCoin(it)
+        topFourCoins = coins.coins.sortedBy {
+            distanceToCoin(it, currLocation)
         }.filter {
             !it.collected
-        }.apply {
-            // set currencies
-            try {
-                this[0].currency.apply {
-                    coinType1.text = this
-                    imgCoinIcon1.setImageResource(
-                            stringIconMap.getOrDefault(this, R.drawable.ic_coinz_24dp))
-                }
-                this[1].currency.apply {
-                    coinType2.text = this
-                    imgCoinIcon2.setImageResource(
-                            stringIconMap.getOrDefault(this, R.drawable.ic_coinz_24dp))
-                }
-                this[2].currency.apply {
-                    coinType3.text = this
-                    imgCoinIcon3.setImageResource(
-                            stringIconMap.getOrDefault(this, R.drawable.ic_coinz_24dp))
-                }
-                this[3].currency.apply {
-                    coinType4.text = this
-                    imgCoinIcon4.setImageResource(
-                            stringIconMap.getOrDefault(this, R.drawable.ic_coinz_24dp))
-                }
-            } catch (npe: NullPointerException) {
-                Log.e("Setting currencies", "Something was null $npe")
-            }
+        }.take(4)
 
-            // set values
-            coinValue1.text = this[0].amount.toInt().toString()
-            coinValue2.text = this[1].amount.toInt().toString()
-            coinValue3.text = this[2].amount.toInt().toString()
-            coinValue4.text = this[3].amount.toInt().toString()
-
-            // set distances
-            coinDistance1.text = "${distanceToCoin(this[0]).toInt()} metres"
-            coinDistance2.text = "${distanceToCoin(this[1]).toInt()} metres"
-            coinDistance3.text = "${distanceToCoin(this[2]).toInt()} metres"
-            coinDistance4.text = "${distanceToCoin(this[3]).toInt()} metres"
-
-            // set directions
-            coinDirection1.text = getDirectionToCoin(this[0])
-            coinDirection2.text = getDirectionToCoin(this[1])
-            coinDirection3.text = getDirectionToCoin(this[2])
-            coinDirection4.text = getDirectionToCoin(this[3])
-
-            // set collectable
-            if (this[0].isCollectable(currLocation!!))
-                coinRow1.setBackgroundColor(ContextCompat.getColor(activity!!, R.color.colorPrimary))
-            else
-                coinRow1.setBackgroundColor(Color.TRANSPARENT)
-            if (this[1].isCollectable(currLocation!!))
-                coinRow2.setBackgroundColor(ContextCompat.getColor(activity!!, R.color.colorPrimary))
-            else
-                coinRow2.setBackgroundColor(Color.TRANSPARENT)
-            if (this[2].isCollectable(currLocation!!))
-                coinRow3.setBackgroundColor(ContextCompat.getColor(activity!!, R.color.colorPrimary))
-            else
-                coinRow3.setBackgroundColor(Color.TRANSPARENT)
-            if (this[3].isCollectable(currLocation!!))
-                coinRow4.setBackgroundColor(ContextCompat.getColor(activity!!, R.color.colorPrimary))
-            else
-                coinRow4.setBackgroundColor(Color.TRANSPARENT)
-
-            // keep reference to coins
-            topFourCoins = arrayOf(this[0].coinId, this[1].coinId, this[2].coinId, this[3].coinId)
-        }
+        viewAdapter.changeData(topFourCoins)
     }
 
     /**
      * Listens to click events for the four nearest coins
      */
-    override fun onClick(view: View) {
-        when (view) {
-            coinRow1 -> collectCoin(topFourCoins[0])
-            coinRow2 -> collectCoin(topFourCoins[1])
-            coinRow3 -> collectCoin(topFourCoins[2])
-            coinRow4 -> collectCoin(topFourCoins[3])
-        }
+    fun onClick(position: Int) {
+        Log.d(javaClass.simpleName, "Coin $position clicked")
+        collectCoin(topFourCoins[position].coinId)
     }
 
     /**
@@ -242,40 +183,64 @@ class MainFragment : Fragment(), View.OnClickListener {
             toast.show()
         }
     }
+}
 
-    /**
-     * Returns the distance between a coin and the user
-     */
-    private fun distanceToCoin(coin: Coin): Float {
-        if (currLocation == null) return 0f
+/**
+ * A [RecyclerView.Adapter] for the blind mode. Displays a list of the top users
+ */
+class BlindModeAdapter(private var coins: List<Coin>,
+                       private val getLocation: ()->Location,
+                       private val clickCallback: (Int)->Unit,
+                       private val activity: Activity) :
+        RecyclerView.Adapter<BlindModeAdapter.MyViewHolder>() {
+    private val stringIconMap = hashMapOf(
+            "DOLR" to R.drawable.ic_dolr,
+            "SHIL" to R.drawable.ic_shil,
+            "PENY" to R.drawable.ic_peny,
+            "QUID" to R.drawable.ic_quid)
 
-        val loc = currLocation as Location
+    class MyViewHolder(val view: View) : RecyclerView.ViewHolder(view)
 
-        return loc.distanceTo(coin.getLocation())
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): BlindModeAdapter.MyViewHolder {
+        val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.blind_coin_item, parent, false)
+        return MyViewHolder(view)
     }
 
-    /**
-     * Gets the direction to a coin as a string
-     *
-     * Returns based on segments around each of 8 cardinal directions
-     */
-    private fun getDirectionToCoin(coin: Coin): String {
-        if (currLocation == null) return "North"
+    fun changeData(data: List<Coin>) {
+        coins = data
+        notifyDataSetChanged()
+    }
 
-        val loc = currLocation as Location
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val coin = coins[position]
 
-        val bearingDegrees = loc.bearingTo(coin.getLocation())
+        // currency
+        holder.view.findViewById<TextView>(R.id.coinType).text = coin.currency
+        holder.view.findViewById<ImageView>(R.id.imgCoinIcon).setImageResource(
+                stringIconMap.getOrDefault(coin.currency, R.drawable.ic_coinz_24dp)
+        )
 
-        return when {
-            bearingDegrees < 22.5  -> "N"
-            bearingDegrees < 67.5  -> "NE"
-            bearingDegrees < 112.5 -> "E"
-            bearingDegrees < 157.5 -> "SE"
-            bearingDegrees < 202.5 -> "S"
-            bearingDegrees < 247.5 -> "SW"
-            bearingDegrees < 292.5 -> "W"
-            bearingDegrees < 337.5 -> "NW"
-            else -> "N"
+        // set value to int approximation
+        holder.view.findViewById<TextView>(R.id.coinValue).text = coin.amount.toInt().toString()
+
+        // set distance
+        holder.view.findViewById<TextView>(R.id.coinDistance).text = "${distanceToCoin(coin, getLocation()).toInt()} metres"
+
+        // set direction
+        holder.view.findViewById<TextView>(R.id.coinDirection).text = getDirectionToCoin(coin, getLocation())
+
+        // set collectable
+        if (coin.isCollectable(getLocation()))
+            holder.view.setBackgroundColor(ContextCompat.getColor(activity, R.color.colorPrimary))
+        else
+            holder.view.setBackgroundColor(Color.TRANSPARENT)
+
+        holder.view.setOnClickListener {
+            clickCallback(position)
         }
     }
+
+    override fun getItemCount() = coins.size
 }
---
+++ app/src/main/java/com/cameronmacleod/coinz/Utils.kt
@@ -0,0 +1,35 @@
+package com.cameronmacleod.coinz
+
+import android.location.Location
+
+/**
+ * Returns the distance between a coin and the user
+ */
+fun distanceToCoin(coin: Coin, user: Location?): Float {
+    if (user == null) return 0f
+
+    return user.distanceTo(coin.getLocation())
+}
+
+/**
+ * Gets the direction to a coin as a string
+ *
+ * Returns based on segments around each of 8 cardinal directions
+ */
+fun getDirectionToCoin(coin: Coin, user: Location?): String {
+    if (user == null) return "N"
+
+    val bearingDegrees = user.bearingTo(coin.getLocation())
+
+    return when {
+        bearingDegrees < 22.5  -> "N"
+        bearingDegrees < 67.5  -> "NE"
+        bearingDegrees < 112.5 -> "E"
+        bearingDegrees < 157.5 -> "SE"
+        bearingDegrees < 202.5 -> "S"
+        bearingDegrees < 247.5 -> "SW"
+        bearingDegrees < 292.5 -> "W"
+        bearingDegrees < 337.5 -> "NW"
+        else -> "N"
+    }
+}
